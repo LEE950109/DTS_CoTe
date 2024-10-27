810ef9403b8790b2f00900360deb816bb43ff0c7
LEE950109 <72004476+LEE950109@users.noreply.github.com>
2024-10-27 20:19:30 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,20 +1,21 @@
-from collections import deque
+# goal의 전체 길이가 20이하로 제한 되므로 그냥 queue 사용
 
 def solution(cards1, cards2, goal):
-    cards1 = deque(cards1)
-    cards2 = deque(cards2)
-    goal = deque(goal)
     
+    # goal 전체를 탐색하고 cards1과 cards2 중에 같은 곳이 있으면 pop
     while goal:
-        if cards1 and cards1[0] == goal[0]:
-            cards1.popleft()
-            goal.popleft()
-        elif cards2 and cards2[0] == goal[0]:
-            cards2.popleft()
-            goal.popleft()
+        if len(cards1) > 0 and cards1[0] == goal[0]:
+            cards1.pop(0)
+            goal.pop(0)
+            
+        elif len(cards2) > 0 and cards2[0] == goal[0]:
+            cards2.pop(0)
+            goal.pop(0)
+            
         else:
             break
-    
+
+    # 만약 goal이 존재 하지않으면 "Yes", goal이 남아 있으면 "No" 
     if not goal:
         return "Yes"
     else:
