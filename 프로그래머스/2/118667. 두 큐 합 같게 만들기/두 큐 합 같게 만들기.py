487942c18d7ccaaa75f65341dbe64499af5e40dc
LEE950109 <72004476+LEE950109@users.noreply.github.com>
2024-10-28 09:50:19 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 78.02 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -7,16 +7,18 @@ def solution(queue1, queue2):
 
     sum1 = sum(queue1)
     sum2 = sum(queue2)
-    
+
     target = (sum1 + sum2) / 2
 
+    # 두 큐 합이 정수로 나누어지지 않으면 불가능하므로 -1 반환
     if target != int(target):
         return -1
 
     target = int(target)
     t = 0
-    max_op = (len(queue1) + len(queue2)) * 2 
+    max_op = (len(queue1) + len(queue2)) * 2  # 최대 연산 횟수 제한 > 가운데 값을 기준으로 주고 받는 경우의 수 
 
+    # 투 포인터 방식을 사용해 각 큐를 관리
     while t < max_op:
         if sum1 == target:
             return t
