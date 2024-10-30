ef8c58b34880327a9a5d30f46cd8878b912725fe
LEE950109 <72004476+LEE950109@users.noreply.github.com>
2024-10-30 15:13:24 +0900

    [level 2] Title: 오픈채팅방, Time: 182.22 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -2,14 +2,16 @@ def solution(record):
     id_name = {}
     messages = []
     result = []
-
+    
+    # record를 ' ' 기준으로 나눈 후 id와 message를 list에 저장, id_name에 id: name 저장
     for rec in record:
         rec = rec.split()
         messages.append((rec[0], rec[1]))
         
-        if rec[0] != 'Leave':
+        if rec[0] != 'Leave': # leave이면 index out range
             id_name[rec[1]] = rec[2]
-
+    
+    # message를 확인하고 해당 아이디의 닉네임을 id_name에서 출력 
     for message, id_ in messages:
         if message == 'Enter':
             result.append(id_name[id_] + '님이 들어왔습니다.')
