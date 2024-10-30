b14fb66c4902153207c2555acd32b01bc635db6b
LEE950109 <72004476+LEE950109@users.noreply.github.com>
2024-10-30 14:18:49 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,14 +1,17 @@
 def solution(clothes):
     dict_clothes = {}
+    num = 1 # 경우의 수를 담을 변수
+    
+    # {category: number of clothes} 의 형태
     for value, key in clothes:
         if key in dict_clothes:
             dict_clothes[key] += 1
         else:
             dict_clothes[key] = 1
-    print(dict_clothes)
     
-    num = 1
+    # (n+1)(m+1)... > +1: 각 카테고리를 포함하지 않는 경우 0을 포함해서 
     for k in dict_clothes:
         num *= (dict_clothes[k]+1)
     
+    # 옷을 안입는 경우를 제외하고 반환
     return num -1
