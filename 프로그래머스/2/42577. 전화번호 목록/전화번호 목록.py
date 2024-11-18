4bff6a1134c631b85cb808e61c8c5a546c168cc0
LEE950109 <72004476+LEE950109@users.noreply.github.com>
2024-11-18 11:10:43 +0900

    [level 2] Title: 전화번호 목록, Time: 124.30 ms, Memory: 30.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,12 @@
+def solution(phone_book):
+    phone_book.sort()
+
+    for i in range(len(phone_book)-1):
+
+        if phone_book[i][0] == phone_book[i+1][0]:
+            l = len(phone_book[i])
+
+            if phone_book[i] in phone_book[i+1][:l]:
+                return False
+
+    return True
